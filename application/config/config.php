492910b7d35113108e30f128d07914748e124af1
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-09-07 13:15:33 +0700

    asdasdasd

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 
 //
-$config['base_url'] = 'http://localhost/hrd/';
+$config['base_url'] = 'https://hrd.mazhub.com/';
 
 /*
 |--------------------------------------------------------------------------
